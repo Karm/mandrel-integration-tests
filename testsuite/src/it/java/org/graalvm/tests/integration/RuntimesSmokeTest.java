e9eeb7723c8a7ad86f76f2b8757144086c3d731d
Foivos Zakkak <fzakkak@redhat.com>
2024-10-18 15:10:19 +0300

    Start Jaeger to avoid errors related to opentracing/opentelemetry
    Closes https://github.com/Karm/mandrel-integration-tests/issues/290

--- testsuite/src/it/java/org/graalvm/tests/integration/RuntimesSmokeTest.java
+++ testsuite/src/it/java/org/graalvm/tests/integration/RuntimesSmokeTest.java
@@ -55,6 +55,7 @@ import static org.graalvm.tests.integration.utils.Commands.getRSSkB;
 import static org.graalvm.tests.integration.utils.Commands.getRunCommand;
 import static org.graalvm.tests.integration.utils.Commands.parsePort;
 import static org.graalvm.tests.integration.utils.Commands.processStopper;
+import static org.graalvm.tests.integration.utils.Commands.removeContainers;
 import static org.graalvm.tests.integration.utils.Commands.runCommand;
 import static org.graalvm.tests.integration.utils.Commands.stopAllRunningContainers;
 import static org.graalvm.tests.integration.utils.Commands.stopRunningContainer;
@@ -170,6 +171,7 @@ public class RuntimesSmokeTest {
             // it might be valuable to have the binary and not just the logs?
             // Nope: Delete it. One can reproduce it from the journal file we maintain.
             cleanTarget(app);
+            removeContainers("quarkus_jaeger");
         }
     }
 
